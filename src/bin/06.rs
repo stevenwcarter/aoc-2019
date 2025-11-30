6fe6c53ec6e7cc4ca49ef89238057e6422703ab3
Steven Carter <steve@javapl.us>
2025-11-30 15:24:31 -0500

    chore: small clean-ups on day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,17 +1,13 @@
-use std::{
-    fmt::Display,
-    hash::{BuildHasherDefault, DefaultHasher},
-    sync::OnceLock,
-};
+use std::{fmt::Display, hash::DefaultHasher, sync::OnceLock};
 
 use cached::proc_macro::cached;
 use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
 use std::hash::{Hash, Hasher};
 
-use nohash::NoHashHasher;
+use nohash::BuildNoHashHasher;
 
-type BuildNoHash = BuildHasherDefault<NoHashHasher<u64>>;
+type BuildNoHash = BuildNoHashHasher<u64>;
 type OptimizedMap = HashMap<u64, u64, BuildNoHash>;
 
 advent_of_code::solution!(6);
@@ -39,11 +35,13 @@ fn build_orbit_path(target: u64, end: u64, path: &mut Vec<u64>) {
         build_orbit_path(*new_target, end, path);
     }
 }
+
 pub fn calculate_hash<T: Hash + Display>(t: &T) -> u64 {
     let mut s = DefaultHasher::new();
     t.hash(&mut s);
     s.finish()
 }
+
 pub fn part_one(input: &str) -> Option<usize> {
     let map: OptimizedMap = input
         .lines()
@@ -55,13 +53,7 @@ pub fn part_one(input: &str) -> Option<usize> {
 
     PART1_MAP.get_or_init(|| map);
 
-    Some(
-        ends.iter()
-            // .inspect(|e| println!("\n!!!{e}!!!"))
-            .map(|e| count_orbits(*e))
-            // .inspect(|c| println!("Orbit count: {c}"))
-            .sum(),
-    )
+    Some(ends.iter().map(|e| count_orbits(*e)).sum())
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
